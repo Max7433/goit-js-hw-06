d8f469ef6effa70b1a42262e4c0bb640bd12f366
Max7433 <104869880+Max7433@users.noreply.github.com>
2022-10-03 10:09:46 +0300

    hw01 hw02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+const liArray = [];
+ingredients.forEach((ingredients) => {
+  const item = document.createElement("li");
+  item.className = "item";
+  item.textContent = ingredients;
+  liArray.push(item);
+});
+list.append(...liArray);
