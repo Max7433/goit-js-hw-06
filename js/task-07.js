4ec61696a8c1cfcee2a4b8ae9dce07f02a529edf
Max7433 <104869880+Max7433@users.noreply.github.com>
2022-10-03 14:19:41 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("#font-size-control");
 const alfaRomeo = document.querySelector("#text");
 
-input.addEventListener("change", (e) => {
+input.addEventListener("input", (e) => {
   alfaRomeo.style.fontSize = `${e.target.value}px`;
   alfaRomeo.style.color = "#f44336";
   alfaRomeo.style.textTransform = "uppercase";
